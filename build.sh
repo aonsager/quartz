bf28ffbf47ca249a2c834aa5f5909150ca8d2e0d
Alex Onsager <alex.onsager@gmail.com>
2025-12-03 14:09:03 +0900

    Quartz sync: Dec 3, 2025, 2:09 PM

--- build.sh
+++ build.sh
@@ -1,3 +1,33 @@
 #!/bin/bash
+
+DO_SERVE=false
+DO_PUSH=false
+
+# Parse flags
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --serve|-s)
+            DO_SERVE=true
+            ;;
+        --push|-p)
+            DO_PUSH=true
+            ;;
+        *)
+            echo "Unknown option: $1"
+            exit 1
+            ;;
+    esac
+    shift
+done
+
 rsync -av --delete "$CONTENT_SOURCE" content/
-npx quartz build --serve
+
+if [ "$DO_SERVE" = true ]; then
+	npx quartz build --serve
+else
+	npx quartz build
+fi
+
+if [ "$DO_PUSH" = true ]; then
+	npx quartz sync
+fi
